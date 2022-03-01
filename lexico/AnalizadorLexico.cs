8d515f09e63f15382ea3c9623b4526980685b3ca
JORGE ALBERTO JIM MENDOZA <jorge_1319104689@uptecamac.edu.mx>
2022-02-28 22:13:58 -0600

    Validacion

--- lexico/AnalizadorLexico.cs
+++ lexico/AnalizadorLexico.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace lexico
 {
@@ -11,114 +12,158 @@ namespace lexico
         private LinkedList<Token> Salida;
         private int estado;
         private String auxiliarLexico;
+        private int contador;
+        private int parentesisD, parentesisI, LlaveD, LlaveI,CorcheteD,CorcheteI;
 
         public LinkedList<Token> escanear(String entrada)
         {
+            //Se declaran las variables que inicien limpias
             Salida = new LinkedList<Token>();
             estado = 0;
             auxiliarLexico = "";
-
+            contador = 0;
+            parentesisD = 0; parentesisI = 0; LlaveD = 0; LlaveI = 0; CorcheteD = 0;CorcheteI = 0; // Variables para identificar validacion
             Char caracter;
-            for (int i = 0; i < entrada.Length; i++)
+            //Se inicia un siclo para la verificacion de la operacion sea correcta
+            for (int i =0; i<entrada.Length;i++)
             {
                 caracter = entrada.ElementAt(i);
-                switch (estado)
+                if (caracter == '(')//Parentesis Izquierdo
                 {
-                    case 0:
-                        if (caracter == '(')//Parentesis Izquierdo
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.ParentesisIzquierdo);
-                            break;
-                        }
-                        else if (caracter == ')')//Parentesis Derecho
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.ParentesisDerecho);
-                            break;
-                        }
-                        else if (caracter == '{')//LLave Izquierda
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.LlaveIzquierda);
-                            break;
-                        }
-                        else if (caracter == '}')//LLave Derecha
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.LlaveDerecha);
-                            break;
-                        }
-                        else if (caracter == '*')//Operador Multiplicacion
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.OperadorMultiplicador);
-                            break;
-                        }
-                        else if (caracter == '/')//Operador Division
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.OperadorDivision);
-                            break;
-                        }
-                        else if (caracter == '+')//Operador Suma
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.OperadorSuma);
-                            break;
-                        }
-                        else if (caracter == '-')//Operador resta
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.OperadorResta);
-                            break;
-                        }
-                        else if (caracter == '[')//Corchete izquierdo
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.CorcheteIzquierdo);
-                            break;
-                        }
-                        else if (caracter == ']')//Corchete derecho
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.CorcheteDerecho);
-                            break;
-                        }
-                        int variable = (int)Char.GetNumericValue(caracter);// Se cambia a enteros para que valide los errores
-                        if (variable>=0)//Numeros
-                        {
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.Numero);
-                            break;
-                        }
-                        else//Default si no se encontro ninguna coincidencia
-                        {//Marcar error
-                            auxiliarLexico += caracter;
-                            agregarToken(Token.Tipo.Error);
-                        }
-                        break;
-                    case 1://algun error
-                         auxiliarLexico += caracter;
-                        agregarToken(Token.Tipo.Error);
-                        break;
-                    default://rectifiacion si hay error
-                        auxiliarLexico += caracter;
-                        agregarToken(Token.Tipo.Error);
-                        break;
+                    parentesisI += 1;
+                }
+                else if (caracter == ')')//Parentesis Derecho
+                {
+                    parentesisD += 1;
+                }
+                else if (caracter == '{')//LLave Izquierda
+                {
+                    LlaveI += 1;
+                }
+                else if (caracter == '}')//LLave Derecha
+                {
+                    LlaveD += 1;
+                }
+                else if (caracter == '[')//Corchete izquierdo
+                {
+                    CorcheteI += 1;
+                }
+                else if (caracter == ']')//Corchete derecho
+                {
+                    CorcheteD += 1;
                 }
             }
+            int sumaTodos = parentesisD + parentesisI + LlaveD + LlaveI + CorcheteD + CorcheteI;//Suma de todas las llaves encontradas y verificar si es correcta 
+            if ((sumaTodos % 2) == 0)//Se valide que en la suma numero par para asi verificar que todo este bien
+            {
+                //Si se valida se empieza a dividir letra por letra oh caracter y enlistar en una tabla que es lo que se introdujo
+                for (int i = 0; i < entrada.Length; i++)
+                {
+                    caracter = entrada.ElementAt(i);
+
+                    switch (estado)
+                    {
+                        case 0:
+                            if (caracter == '(')//Parentesis Izquierdo
+                            {
+                                auxiliarLexico += caracter;//Se declara el caracter en el auxiliar
+                                agregarToken(Token.Tipo.ParentesisIzquierdo);//agrega a la lista de la tabla
+                                break;//Cierra el switch
+                            }
+                            else if (caracter == ')')//Parentesis Derecho
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.ParentesisDerecho);
+                                break;
+                            }
+                            else if (caracter == '{')//LLave Izquierda
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.LlaveIzquierda);
+                                break;
+                            }
+                            else if (caracter == '}')//LLave Derecha
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.LlaveDerecha);
+                                break;
+                            }
+                            else if (caracter == '*')//Operador Multiplicacion
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.OperadorMultiplicador);
+                                break;
+                            }
+                            else if (caracter == '/')//Operador Division
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.OperadorDivision);
+                                break;
+                            }
+                            else if (caracter == '+')//Operador Suma
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.OperadorSuma);
+                                break;
+                            }
+                            else if (caracter == '-')//Operador resta
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.OperadorResta);
+                                break;
+                            }
+                            else if (caracter == '[')//Corchete izquierdo
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.CorcheteIzquierdo);
+                                break;
+                            }
+                            else if (caracter == ']')//Corchete derecho
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.CorcheteDerecho);
+                                break;
+                            }
+                            int variable = (int)Char.GetNumericValue(caracter);// Se cambia a enteros para que valide los errores
+                            if (variable >= 0)//Numeros
+                            {
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.Numero);
+                                break;
+                            }
+                            else//Default si no se encontro ninguna coincidencia
+                            {//Marcar error
+                                auxiliarLexico += caracter;
+                                agregarToken(Token.Tipo.Error);
+                            }
+                            break;
+                        case 1://algun error
+                            auxiliarLexico += caracter;
+                            agregarToken(Token.Tipo.Error);
+                            break;
+                        default://rectifiacion si hay error
+                            auxiliarLexico += caracter;
+                            agregarToken(Token.Tipo.Error);
+                            break;
+                    }
+                }
+            }
+            else//Si no manda un msg de verificacion de la operacion
+            {
+                MessageBox.Show("Verificar que la operacion sea correcta", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
             return Salida;
 
         }
 
-        public void agregarToken(Token.Tipo tipo)
+        public void agregarToken(Token.Tipo tipo)//Agrego tokens a la lista
         {
             Salida.AddLast(new Token(tipo, auxiliarLexico));
             auxiliarLexico = "";
             estado = 0;
         }
-        public void imprimirListaToken(LinkedList<Token> lista)
+        public void imprimirListaToken(LinkedList<Token> lista)//Se imprime en consola los resultados
         {
             foreach (Token item in lista)
             {
--- lexico/Form1.cs
+++ lexico/Form1.cs
@@ -15,46 +15,46 @@ namespace lexico
         public Form1()
         {
             InitializeComponent();
-            button2.Enabled = false;
-            button2.BackColor = Color.Red;
+            button2.Enabled = false;//Se inicia con el boton 2 que es de reinicio para evitar algun error
+            button2.BackColor = Color.Red;//Se le coloca el color rojo al boton
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//Boton de inicio
         {
-            if (textBox1.Text=="")
+            if (textBox1.Text=="")//Valido que el campo de texto este vacio
             {
                 MessageBox.Show("Campos Vacios","Alerta");
             }
-            else
+            else//si no es vacio se inicia el analizador
             {
-                dataGridView1.Rows.Clear();
-                String entrada = textBox1.Text;
-                AnalizadorLexico lex = new AnalizadorLexico();
-                LinkedList<Token> ltokens = lex.escanear(entrada);
-                foreach (Token token in ltokens)
+                dataGridView1.Rows.Clear();//vacio la tabla
+                String entrada = textBox1.Text;//Decalro el texto ingresado en un string
+                AnalizadorLexico lex = new AnalizadorLexico();//Se manda a llamar el objeto
+                LinkedList<Token> ltokens = lex.escanear(entrada);//Se crea una lista con TOKEN y se ingresa en el objeto
+                foreach (Token token in ltokens)//Se Ingresan todos los datos en la tabla
                 {
                     dataGridView1.Rows.Add(token.Getval(), token.GetTipo(), token.GetID());
                 }
-                lex.imprimirListaToken(ltokens);
-                button1.Enabled = false;
-                button1.BackColor = Color.Red;
-                button2.Enabled = true;
-                button2.BackColor = Color.YellowGreen;
-                textBox1.Enabled = false;
+                lex.imprimirListaToken(ltokens);//Se colocan los datos en la tabla
+                button1.Enabled = false;//Desactivo el boton iniciar
+                button1.BackColor = Color.Red;//Se coloca en color rojo el boton iniciar
+                button2.Enabled = true;//Habilito el boton reiniciar
+                button2.BackColor = Color.YellowGreen;//Se coloca en color verde el boton de reiniciar
+                textBox1.Enabled = false;//Se desabilita el campo del texto para mejor experiencia de usuario
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)//Boton de reinicio
         {
-            dataGridView1.Rows.Clear();
-            textBox1.Text = "";
-            button1.Enabled = true;
-            button1.BackColor = Color.GreenYellow;
-            button2.Enabled = false;
-            button2.BackColor = Color.Red;
-            textBox1.Enabled = true;
+            dataGridView1.Rows.Clear();//Se vacia la tabla
+            textBox1.Text = "";//Se vacia el campo de texto
+            button1.Enabled = true;//Habilita el boton de iniciar
+            button1.BackColor = Color.GreenYellow;//Coloca el color verde
+            button2.Enabled = false;//Desabilito el boton de reinicio
+            button2.BackColor = Color.Red;//Coloco el color rojo en el boton de reinicio
+            textBox1.Enabled = true;//Habilito el campo de texto
         }
-
+        
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
